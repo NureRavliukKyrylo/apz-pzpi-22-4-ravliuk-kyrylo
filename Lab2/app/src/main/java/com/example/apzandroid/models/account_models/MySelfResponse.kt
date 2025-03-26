718b73f652d4fd456ae18f4852673946c4bb5b7a
NureRavliukKyrylo <kyrylo.ravliuk@nure.ua>
2025-03-26 19:04:59 +0100

    some issues

---
+++ Lab2/app/src/main/java/com/example/apzandroid/models/account_models/MySelfResponse.kt
@@ -0,0 +1,8 @@
+package com.example.apzandroid.models.account_models
+
+data class MySelfResponse (
+    val username: Int,
+    val email: String,
+    val role: Int,
+    val date_joined: String 
+)
